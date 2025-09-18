e19dc510f856def9c47d6417559fcfbd1e65ac7d
AlicanONI.Kaya <128067349+AlicanKaya192@users.noreply.github.com>
2025-09-18 04:03:08 +0300

    Time: 0 ms (100.00%) | Memory: 39 MB (80.91%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cs
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -1,7 +1,18 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        int index = haystack.IndexOf(needle);
+        if(needle.Length == 0) return 0;
 
-        return index;
+        for(int i = 0; i <= haystack.Length - needle.Length; i++){
+            bool found = true;
+
+            for(int j = 0; j < needle.Length; j++){
+                if (haystack[i+j] != needle[j]){
+                    found = false;
+                    break;
+                }
+            }
+            if(found) return i;
+        }
+        return -1;
     }
 }
